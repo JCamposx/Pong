9194cee532d694d110891e301ee1d84c367cdc39
L34324 <L34324@lab.aca>
2023-04-19 19:07:46 -0500

    add ball collision with paddles

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    public float Speed = 1f;
+    public Vector2 Speed;
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = transform.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(Speed.x, Speed.y);
     }
 
     // Update is called once per frame
@@ -30,6 +32,12 @@ public class BallMovement : MonoBehaviour
         // Vector3 unitRight = new Vector3(1f, 0f, 0f);
 
         // This does the same of all what is above
-        transform.position += Vector3.right * Speed * Time.deltaTime;
+        // transform.position += Vector3.right * Speed * Time.deltaTime;
+        // Can't use this because Speed changed to Vector
+    }
+
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        Speed *= -1f;
     }
 }
