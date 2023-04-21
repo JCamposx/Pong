98ba44eaadd421f8b67b3db158effe5a0e81cee4
L35224 <L35224@lab.aca>
2023-04-21 16:17:43 -0500

    add gameplay finished

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -17,27 +17,24 @@ public class BallMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Access to the position of the Game Object (the ball)
-        // Vector3 position = transform.position;
-
-        // Vector3 newPosition = new Vector3(
-        //     position.x + (Speed * Time.deltaTime),
-        //     position.y,
-        //     position.z
-        // );
-
-        // transform.position = newPosition;
-
-        // Vector3.right is what is below
-        // Vector3 unitRight = new Vector3(1f, 0f, 0f);
-
-        // This does the same of all what is above
-        // transform.position += Vector3.right * Speed * Time.deltaTime;
-        // Can't use this because Speed changed to Vector
+        rb.velocity = new Vector2(Speed.x, Speed.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collider)
     {
-        Speed *= -1f;
+        if (collider.transform.CompareTag("Wall"))
+        {
+            Speed.y *= -1f;
+        }
+        else
+        {
+            Speed.y = Random.Range(5f, -5f);
+            Speed.x *= -1f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        transform.position = new Vector3(0f, 0f, 0f);
     }
 }
